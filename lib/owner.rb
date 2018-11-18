791fdc34a259c8c78b457509f20e65ee116ada83
Justin Beaulieu <beaulieudev@gmail.com>
2018-11-18 22:00:33 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -29,5 +29,36 @@ class Owner
   def say_species
   end
 
+  def buy_fish(name)
 
+  end
+
+  def buy_cat(name)
+
+  end
+
+  def buy_dog(name)
+
+  end
+
+  def walks_dogs
+
+  end
+
+  def play_with_cats
+
+  end
+
+  def feed_fish
+
+  end
+
+  def sells_pets
+  end
+
+  def lists_pets
+
+  end
+
+  
 end
