5094246285742e86e232359ce48d949a584cd864
Justin Beaulieu <beaulieudev@gmail.com>
2018-11-18 21:48:36 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,9 +1,13 @@
 class Owner
   attr_reader :species
   attr_accessor :name
-
+  @@all
   def initialize(species)
     @species = species
     @name = name
+  end
+
+  def self.all?
+    @@all
   end
 end
